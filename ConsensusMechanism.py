4990437443c0a881f1ff67c26d743dd948e80138
Jack Peterson <jack@tinybike.net>
2014-09-25 14:24:29 -0700

    Update to use pyconsensus module

--- ConsensusMechanism.py
+++ ConsensusMechanism.py
@@ -1,8 +1,9 @@
 """a python version of the R program at https://github.com/psztorc/Truthcoin
 """
-#import python_ConsensusMechanism as consensus
-import numpy, custommath, consensus, tools
-#import python_CustomMath as custommath
+from __future__ import division
+import numpy
+import tools
+from pyconsensus import Oracle
 
 def keep_nums(t):
     if type(t)==list:
@@ -15,17 +16,28 @@ def keep_nums(t):
     else:
         return True
 
+def GetWeight(Vec, AddMean=0):
+    """Takes an array (vector in practice), and returns proportional distance from zero."""
+    New = abs(Vec)       #Absolute Value
+    if AddMean == 1:     #Add the mean to each element of the vector
+        New = New + mean(New)
+    if sum(New) == 0:    #Catch an error here
+        New = New + 1
+    New = New/sum(New)   #Normalize
+    return(New)
+
+
 def main(m, weights):
-    weights=numpy.array(weights)
-    weights=custommath.GetWeight(weights)
-    a=numpy.array(m)
-    k=keep_nums(m)
-    a=numpy.ma.masked_array(a, mask=k)
-    a=consensus.Factory(a, Rep=weights)
+    weights = GetWeight(numpy.array(weights))
+    a = numpy.array(m)
+    k = keep_nums(m)
+    a = numpy.ma.masked_array(a, mask=k)
+    oracle = Oracle(votes=a, weights=weights)
+    a = oracle.consensus()
     return {'outcome':a['Decisions']['DecisionOutcome_Final'],
             'author_bonus':a['Decisions']['Author Bonus'],
             'participation':a['Participation'],
-            'certainty':map(lambda x: x[0], a['Decisions']['Certainty']),
+            'certainty': numpy.array(a['Decisions']['Certainty']),
             'votecoin_bonus_for_voters':a['Agents']['SmoothRep'],
             'truthcoin_bonus_for_voters':a['Agents']['RowBonus']}
 
