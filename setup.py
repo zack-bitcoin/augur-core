8c068ca9c58fa10161cafc154b31bb209d59960f
Jack Peterson <jack@tinybike.net>
2014-09-25 18:51:18 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,12 @@ except ImportError:
 setup(
     name="Truthcoin-POW",
     version="0.1",
-    description="Even-more-experimental implementation of Truthcoin",
+    description="Truthcoin-in-Python",
     author="Zack Hess",
     author_email="<zack@dyffy.com>",
     maintainer="Zack Hess",
     maintainer_email="<zack@dyffy.com>",
-    url="https://github.com/tensorjack/skunkworks",
-    packages=["skunkworks"],
+    url="https://github.com/zack-bitcoin/Truthcoin-POW",
     install_requires=["numpy", "pyconsensus"],
     keywords = ["consensus", "prediction market", "PM", "truthcoin", "oracle"]
 )
