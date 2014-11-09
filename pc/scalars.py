6f6f82af1b20132dd7bbfea524306dd5d1e2c514
zack-bitcoin <zack.bitcoin@gmail.com>
2014-11-09 12:59:29 -0800

    another scalars bug

--- pc/scalars.py
+++ pc/scalars.py
@@ -1,11 +1,12 @@
 from cdecimal import Decimal
 pi=Decimal('3.141592653589793')
+E=Decimal('2.718281828459045')
 def dec_greater_than(a, b): return float(a)>float(b)
 def factorial(n): return 1 if not dec_greater_than(n, 1) else n*factorial(n-1)
 def alternate_sum(v, positive=1, s=0): return s if len(v)==0 else alternate_sum(v[1:], -positive, s+v[0]*positive)
 Taylor_series_depth=13
 def trig(x, t):
-    if not dec_greater_than(x, Decimal('0.0000001')) and dec_greate_than(x, Decimal('-0.0000001')): return 1-t
+    if not dec_greater_than(x, Decimal('0.0000001')) and dec_greater_than(x, Decimal('-0.0000001')): return 1-t
     if not dec_greater_than(x, -pi): return trig(x+2*pi, t)
     if dec_greater_than(x, pi): return trig(x-2*pi, t)
     a=filter(lambda y: y%2==t, range(Taylor_series_depth))
@@ -31,10 +32,13 @@ def plug_in(x, v):
     return sum(c)
 def integrate(v, m=100): return sum(map(lambda x: plug_in(Decimal(1)*x/100, v), range(m)))
 def C(v, B): 
-    a=integrate(map(lambda x: x/B, v))
+    a=integrate(map(lambda x: E**(x/B), v))
     return B*a.ln()
 def cost_to_buy_shares(current_state, shares, B):
     a=C(current_state, B)
     b=C(map(add, current_state, shares), B)
     return b-a
+
 print(cost_to_buy_shares([Decimal('10'),Decimal('50'),0,0,0], [Decimal('10'),0,0,0,0], 1000))
+print(cost_to_buy_shares([Decimal('10'),Decimal('50'),0,0,0], [Decimal('5'),0,0,0,0], 1000))
+print(cost_to_buy_shares([Decimal('10'),Decimal('50'),0,0,0], [Decimal('5'),Decimal('5'),0,0,0], 1000))
