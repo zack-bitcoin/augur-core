044662dd6468d38a4894e1009645ce8906d42df7
zack <zack.bitcoin@gmail.com>
2014-12-26 11:05:25 -0600

    removed unnecessary higher-order functions

--- script.py
+++ script.py
@@ -1,39 +1,35 @@
 import copy
 import tools
-def apply_script(f, stack):#this function is a tiny forth-like language.
+def forth(f, stack): 
     do={'swap':(lambda l: [l[1]]+[l[0]]+l[2:]), 
         'drop':(lambda l: l[1:]),
         '*':(lambda l: [l[0]*l[1]]+l[2:]),
         '/':(lambda l: [l[0]/l[1]]+l[2:]),
         '-':(lambda l: [l[0]-l[1]]+l[2:]),
         '+':(lambda l: [l[0]+l[1]]+l[2:])}
-    def helper(f, stack): 
-        tools.log('stack: ' +str(stack))
-        if len(f)==0: return stack
-        try:
-            f[0]=float(f[0])
-            return helper(f[1:], [f[0]]+stack)
-        except Exception as exc:
-            tools.log(exc)
-            return helper(f[1:], do[f[0]](stack))
-    return helper(f.split(' '), stack)
+    if len(f)==0: return stack
+    try:
+        f[0]=float(f[0])
+        return forth(f[1:], [f[0]]+stack)
+    except Exception as exc:
+        tools.log(exc)
+        return forth(f[1:], do[f[0]](stack))
+def apply_script(f, stack):#this function is a tiny forth-like language.
+    return forth(f.split(' '), copy.deepcopy(stack))
+def share_value_helper(stack, algorithm, functions, state):
+    ratio=apply_script(functions[algorithm[0]], stack)[0]
+    if state in algorithm[1]: 
+        if len(algorithm[1])==1:
+            return ratio
+        n=2
+    else: 
+        ratio=1-ratio
+        if len(algorithm)==2:
+            return ratio
+        n=3
+    return ratio*share_value_helper(stack, algorithm[n], functions, state)
 def share_value(pm, state):
-    #out=dict(map(lambda x: [x, ''], pm['output_state']))
-    def helper(stack, algorithm):
-        stack_copy=copy.deepcopy(stack)
-        stack_copy=apply_script(pm['functions'][algorithm[0]], stack_copy)
-        if state in algorithm[1]: 
-            s=stack_copy[0]
-            if len(algorithm[1])==1:
-                return s
-            n=2
-        else: 
-            s=1-stack_copy[0]
-            if len(algorithm)==2:
-                return s
-            n=3
-        return s*helper(stack, algorithm[n])
-    return helper(pm['results'], pm['algorithm'])
+    return share_value_helper(pm['results'], pm['algorithm'], pm['functions'], state)
 def test():
     example_pm={'results':[1,0], 'predictions':['obama wins election', 'pinto beans above 80 cents'], 'functions':['swap drop', 'drop'], 'output states':['a', 'b', 'c', 'd'], 'algorithm':[0, ['a', 'b'], [1, ['a']], [1, ['c']]]}
     print(share_value(example_pm, 'a'))
